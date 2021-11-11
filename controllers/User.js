e8f6249d3b91fa420a2508a366f25a3c7a0e28e0
Carl Angelo Limpio <carlangelolimpio@gmail.com>
2021-11-11 20:14:48 +0800

    Added recent progress

--- controllers/User.js
+++ controllers/User.js
@@ -54,35 +54,56 @@ module.exports.setAdmin = (reqParams, reqBody) => {
 };
 
 // checkout
-// module.exports.checkout = async (data) => {
-//   let addToCart = await User.findById(data.userId).then((user) => {
-//     user.cart.push({ productId: data.productId, quantity: data.quantity });
-//     return user.save().then((user, err) => {
-//       if (err) {
-//         return false;
-//       } else {
-//         return true;
-//       }
-//     });
-//   });
-//   let processOrder = await Product.findById(data.productId).then((product) => {
-//     product.order.push({
-//       userId: data.userId,
-//       quantity: data.quantity,
-//       totalAmount: product.price * data.quantity,
-//     });
+module.exports.checkout = async (data, productData) => {
+  let addToCart = await User.findById(data.userId).then((user) => {
+    user.cart.push({
+      productId: data.productId,
+      quantity: data.quantity,
+      totalAmount: productData.price * data.quantity,
+    });
+    return user.save().then((user, err) => {
+      if (err) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  });
+  let processOrder = await Product.findById(data.productId).then((product) => {
+    product.order.push({
+      userId: data.userId,
+      quantity: data.quantity,
+      totalAmount: productData.price * data.quantity,
+    });
+    return product.save().then((product, err) => {
+      if (err) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  });
+  if (addToCart && processOrder) {
+    return "Order successfully processed.";
+  } else {
+    return false;
+  }
+};
 
-//     return product.save().then((product, err) => {
-//       if (err) {
-//         return false;
-//       } else {
-//         return true;
-//       }
-//     });
-//   });
-//   if (addToCart && processOrder) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
+// retrieving all orders; admin only
+module.exports.getAllOrders = () => {
+  return User.find({})
+    .select("cart")
+    .then((result) => {
+      return result;
+    });
+};
+
+// retrieving authenticated user orders
+module.exports.getUserOrders = (user) => {
+  return User.findById(user.id)
+    .select("cart -_id")
+    .then((result) => {
+      return result;
+    });
+};
--- routes/User.js
+++ routes/User.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/User");
 const auth = require("../auth");
+const Product = require("../models/Product");
 
 // register
 router.post("/register", (req, res) => {
@@ -25,20 +26,50 @@ router.put("/:userId/setAsAdmin", auth.verify, (req, res) => {
       .setAdmin(req.params, req.body)
       .then((resultFromController) => res.send(resultFromController));
   } else {
-    res.send("Not authorized to set admin");
+    res.send("Unauthorized to set admin");
   }
 });
 
-// router.post("/checkout", auth.verify, (req, res) => {
-//   const userData = auth.decode(req.headers.authorization);
-//   let data = {
-//     userId: req.body.userId,
-//     productId: req.body.productId,
-//     quantity: req.body.quantity,
-//   };
-//   userController
-//     .checkout(data)
-//     .then((resultFromController) => res.send(resultFromController));
-// });
+// creating an order; authenticated user only
+router.post("/checkout/", auth.verify, async (req, res) => {
+  const userData = auth.decode(req.headers.authorization);
+  if (userData.isAdmin == false) {
+    let data = {
+      userId: req.body.userId,
+      productId: req.body.productId,
+      quantity: req.body.quantity,
+    };
+    const product = await Product.findById(req.body.productId);
+    userController
+      .checkout(data, product)
+      .then((resultFromController) => res.send(resultFromController));
+  } else {
+    res.send("Admin cannot create order for user.");
+  }
+});
+
+// retrieving all orders; admin only
+router.get("/orders", auth.verify, (req, res) => {
+  const userData = auth.decode(req.headers.authorization);
+  if (userData.isAdmin) {
+    userController
+      .getAllOrders()
+      .then((resultFromController) => res.send(resultFromController));
+  } else {
+    res.send("No Access!");
+  }
+});
+
+// retrieve authenticated user orders
+router.get("/myOrders", auth.verify, (req, res) => {
+  const userData = auth.decode(req.headers.authorization);
+  if (userData.isAdmin == false) {
+    userController
+      .getUserOrders(userData)
+      .then((resultFromController) => res.send(resultFromController));
+  } else {
+    res.send("No Access!");
+  }
+});
 
 module.exports = router;
